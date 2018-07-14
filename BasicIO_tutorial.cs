93568e78a318b87f5a7e06ff8ced14ddaaa89253
Nishant Baheti <nishantbaheti.it19@gmail.com>
2018-07-14 22:36:51 +0530

    Hackerearth

---
+++ BasicIO_tutorial.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Solution{
+    static void Main(String[] args){
+        int N=Convert.ToInt32(Console.ReadLine());
+        String S=Console.ReadLine();
+        Console.WriteLine("{0}",N*2);
+        Console.WriteLine("{0}",S);
+    }
+}
