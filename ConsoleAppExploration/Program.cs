294dc52391c2c796bb2746af22b5466d1b16fb41
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-10-19 09:43:02 +0530

    Pregress commit

--- ConsoleAppExploration/Program.cs
+++ ConsoleAppExploration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace ConsoleAppExploration
 {
@@ -19,21 +21,68 @@ namespace ConsoleAppExploration
                 Console.WriteLine("Either sum is less than 10 or a is not greater than b");
             }
         }
+        
+        static void exploreFor(int n)
+        {
+            var fibonacciNumber = new List<int> { 1, 1 };
+            int prevNumber1 = 0;
+            int prevNumber2 = 0;
+            for(int i = 0; i <= n; i++)
+            {
+                prevNumber1 = fibonacciNumber[fibonacciNumber.Count - 1];
+                prevNumber2 = fibonacciNumber[fibonacciNumber.Count - 2];
+
+                fibonacciNumber.Add(prevNumber1 + prevNumber2);
+            }
+
+            foreach(var number in fibonacciNumber)
+            {
+                Console.WriteLine($"{number}");
+            }
+        }
+
+        static void exploreWhile(int n)
+        {
+            var fibonacciNumber = new List<int> { 1, 1 };
+            int prevNumber1 = 0;
+            int prevNumber2 = 0;
+            int i = 0;
+            while(i <= n){
+                prevNumber1 = fibonacciNumber[fibonacciNumber.Count - 1];
+                prevNumber2 = fibonacciNumber[fibonacciNumber.Count - 2];
+
+                fibonacciNumber.Add(prevNumber1 + prevNumber2);
+
+                i += 1;
+            }
+
+            foreach (var number in fibonacciNumber)
+            {
+                Console.WriteLine($"{number}");
+            }
+        }
         static void Main(string[] args)
         {
             //Printing Hello World
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
 
             //Branching 
-            //int a = 5;
-            //int b = 6;
-            //if (a + b > 10)
-            //    Console.WriteLine("Greater than 10");
-            //else
-            //    Console.WriteLine("Less than 10");
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("Greater than 10");
+            else
+                Console.WriteLine("Less than 10");
 
             //Testing function
             exploreIf();
+
+            // loops
+            // for loop
+            exploreFor(20);
+
+            // while loop
+            exploreWhile(20);
         }
     }
 }
