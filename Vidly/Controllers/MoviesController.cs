bf7ca2e71cb6e168d100eb3c0b5dbeac59289e87
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-10-31 23:02:51 +0530

    Progress commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        // want to see random movie
+        // /movies/random 
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Avengers" };
+            return View(movie);
+        }
+    }
+}
